22f6aee7ea478a71b45b30446dfaf76c72a40b9b
peter <seyipeterson@gmail.com>
2022-11-29 14:39:05 +0100

    changed font size

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
         <div className="drop">
           <div className="relative flex flex-col justify-center items-center text-center p-[40px] gap-[15px]">
-            <h1 className='relative text-[#333] text-[1.5em]'>Sign in</h1>
+            <h1 className='relative text-[#333] text-[1.5em] font-bold'>Sign in</h1>
             <form action="" className='flex flex-col gap-[20px]'>
               <div className='inputbox'>
                 <input className='' type="email" name="" id="" placeholder='Email'/>
